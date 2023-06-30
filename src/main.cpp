1e45e95328b8e70979018642604b8368fc9ad386
forresttheman <96036883+forresttheman@users.noreply.github.com>
2023-06-30 12:23:03 -0700

    rotate_modules now selects optimal rotation goal
    - added check in rotate modules to determine if rotation to the reflection (or flip) of the original goal & reversing motor power is quicker than rotation to the original goal, and selects whichever method is faster.
    
    - every power motor has it's reverse status stored in an array now

--- src/main.cpp
+++ src/main.cpp
@@ -79,26 +79,51 @@ void autonomous() {}
  * task, not resume it from where it left off.
  */
 
-// apply a voltage to each turning motor proportional to their distance from the 
-// target rotation, scaled by 5.5 for the physical gearing of the bot.
+// store reverse status of every power motor
+bool power_motor_reverse_status[] = {false, false, false};
+
+void apply_power_motor_reverse(bool value, int index){
+	power_motor_reverse_status[index] = value;
+
+	pros::Motor& power_motor = primary_motors[index];
+	power_motor.set_reversed(value);
+	
+	// print the array values HORRIBLE! why can't you just print whole array :(
+	for (size_t i = 0; i < sizeof(power_motor_reverse_status); i++){
+		pros::lcd::print(3 + i, "reverse status of motor %d", i);
+		pros::lcd::print(4 + i, "is equal to %d", power_motor_reverse_status[i]);
+	}
+}
 
 void rotate_modules(double goal) {
     for (size_t i = 0; i < angle_motors.size(); i++) { 
-        pros::Motor& motor = angle_motors[i]; 
+        pros::Motor& angle_motor = angle_motors[i]; 
 		// Motor& lets us directly modify the actual motor's variables
-		
-		double current_position = motor.get_position() * 5.5;
-        double error = goal - current_position;
 
-		// set proportional voltage
+		// CALCULATE CLOSEST ROTATION GOAL
+		double current_position_geared = angle_motor.get_position() *5.5;
+		bool reverse_power_motor = power_motor_reverse_status[i];
+
+		// compare the two different goals & pick which is closer to position
+		double flipped_goal = goal + 180;
+		double goal_after_check = goal;
+
+		// if our original goal is farther way from current position than the flipped goal, pick the flipped goal
+		if (abs(int(goal - current_position_geared)) > abs(int(flipped_goal - current_position_geared))) {
+			goal_after_check = flipped_goal;
+			reverse_power_motor = !reverse_power_motor;
+		}
+		// otherwise, just use the original goal
+
+		// apply reverse status to array & motor for later reference
+		apply_power_motor_reverse(reverse_power_motor, i);
+
+
+		// ROTATE MODULES
+		double error = goal_after_check - current_position_geared;
 		int proportional_voltage = int((127.0/(90.0*5.5)) * error);
-        motor = proportional_voltage;
 
-		pros::lcd::print(2, "(motor)  raw pos: %f", motor.get_position());
-		pros::lcd::print(3, "(geared) pos: %f", current_position);
-		pros::lcd::print(4, "(geared) goal: %f", goal);
-		pros::lcd::print(5, "(geared) error: %f", error);
-		pros::lcd::print(6, "proportional voltage: %d", proportional_voltage);
+		angle_motor = proportional_voltage;
 	}
 }
 
@@ -116,30 +141,32 @@ void opcontrol() {
 
 
 	while (true) {
-		// grab stick inputs
+		// get stick inputs
 		float left_y = float(controller.get_analog(ANALOG_LEFT_Y));
 		float left_x = float(controller.get_analog(ANALOG_LEFT_X));
 
-		// polar coordinates! 
-		// use pythagorean theorem to calculate magnitude (r) from rect coords
+		// polar coordinates! pyth theorem to get magnitude from (x,y)
 		translate_magnitude = sqrt(pow(left_x,2)+pow(left_y,2));
 		
 		// deadzone (magnitude < 10, do not rotate)
 		if (abs(int(translate_magnitude)) > 10){
 			if ((left_y != 0) || (left_x != 0)){
-				// get theta from arctan function using rect coords
+				// get theta from arctan2 function using rect coords
 				// then convert radian result to degrees
 				// and translate the result by ? degrees to make the front of the bot 0
-				translate_direction = ((180/PI)*atan2(left_y,left_x))+?;
+				translate_direction = ((180/PI)*atan2(left_y,left_x))+0;
 			}
 			// else: do not update direction
 		}
 
+		// apply values to motors
+		rotate_modules(translate_direction*5.5); // physical gear ratio of 5.5:1
 		primary_motors.move_velocity(translate_magnitude);
-		rotate_modules(translate_direction*5.5);
-		// multiply by 5.5 bcs of physical gear ratio of 5.5:1
 
+		// print values to brain
 		pros::lcd::print(1, "(motor space) translate dir: %f", translate_direction);
+		pros::lcd::print(2, "(geared space) translate dir: %f", translate_direction*5.5);
+
 		pros::delay(20);
 	}
 }
